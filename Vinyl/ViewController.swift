54bd6c63389ccc65f29a8ee065a319907664e73d
Matthew Cibulka <mcibulkadev@gmail.com>
2015-01-17 14:37:56 -0500

    Initial Commit

---
+++ Vinyl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vinyl
+//
+//  Created by Matthew Cibulka on 2015-01-17.
+//  Copyright (c) 2015 Matthew Cibulka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
