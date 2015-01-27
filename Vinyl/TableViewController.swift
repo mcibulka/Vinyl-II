c9bca515be3c230df0e5a1ca2a1caf22c5522509
Clayton <rosey109@gmail.com>
2015-01-27 13:42:28 -0500

    manually merged the saving to file and reading from file functionality

--- Vinyl/TableViewController.swift
+++ Vinyl/TableViewController.swift
@@ -19,6 +19,9 @@ class TableViewController: NSTableView
 {
     @IBOutlet weak var TableView: NSTableView!
     
+    func updateTable (){
+        var tableArray = songArray
+    }
 //    var tableArray = [Song(
 //        album: "Group Therapy",
 //        albumArtist: "Above & Beyond",
